bc1926e10d2a435f9932aedf02ab4b2faffe3a3d
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-02-03 19:05:32 +0000

    yup

--- ext/voice.py
+++ ext/voice.py
@@ -8,12 +8,17 @@ class Voice(commands.Cog):
     
     @commands.Cog.listener()
     async def on_voice_join(self, member, state):
-        vc = (self.bot.guild_config.get(state.channel.guild.id, None)).get("voice_channel", None)
+        print("helplo")
+        vc = self.bot.guild_config.get(state.channel.guild.id, None).get("voice_channel", None)
+        print("hehe")
         if not vc or state.channel.id != vc:
+            print("if not")
             return
         
         guild = vc.guild
-        new_vc = await guild.create_voice_channel(name=member.display_name, category=await guild.get_channel((self.bot.guild_config.get(state.channel.guild.id, None).get("voice_category", None))), reason="Made by the personal voice chat module")
+        print("hi")
+        new_vc = await guild.create_voice_channel(name=member.display_name, category=await guild.get_channel(self.bot.guild_config.get(state.channel.guild.id, None).get("voice_category", None)), reason="Made by the personal voice chat module")
+        print("hm")
         await member.move(channel=new_vc)
 
 async def setup(bot: AloneBot):

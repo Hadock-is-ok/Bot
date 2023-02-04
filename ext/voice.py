883492670b8e217cdd0c56eee82c6581b21722b0
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-02-04 15:21:43 +0000

    ye

--- ext/voice.py
+++ ext/voice.py
@@ -1,6 +1,9 @@
-import discord
+import discord, asyncio
 from discord.ext import commands
 from utils.bot import AloneBot
+
+def channel_check(member, state):
+    return state.channel == channel
 
 class Voice(commands.Cog):
     def __init__(self, bot: AloneBot):
@@ -18,8 +21,22 @@ class Voice(commands.Cog):
         await self.bot.db.execute("INSERT INTO voice VALUES ($1, $2, $3)", member.guild.id, member.id, new_vc.id)
         await member.send("Welcome to your own voice chat! Here, you lay the rules. your house, your magic. Have fun!")
     
-    #@commands.Cog.listener()
-    #async def on_voice_leave(self, member, state):
+    @commands.Cog.listener()
+    async def on_voice_leave(self, member, state):
+        vc = self.bot.guild_config.get(state.guild.id, {}).get("community_voice_channels", [])
+        if not vc or not state.channel.id in vc:
+            return
+        
+        if len(state.channel.members) == 0:
+            channel = state.channel
+            try:
+                await self.bot.wait_for("voice_join", timeout=300, check=channel_check)
+            except asyncio.TimeoutError:
+                try:
+                    await channel.delete()
+                except Exception:
+                    pass
+
 
 async def setup(bot: AloneBot):
     await bot.add_cog(Voice(bot))

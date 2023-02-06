8f2aa06d985a55c5c19559f8227be4657e3f86a8
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-02-06 23:55:21 +0000

    holy shit

--- schema.sql
+++ schema.sql
@@ -2,20 +2,33 @@ CREATE TABLE IF NOT EXISTS prefix (
     user_id BIGINT,
     prefix TEXT
 )
+
 CREATE TABLE IF NOT EXISTS guilds (
     guild_id BIGINT PRIMARY KEY,
-    prefix TEXT
+    prefix TEXT,
+    voice_channel BIGINT,
+    voice_category BIGINT
 )
+
 CREATE TABLE IF NOT EXISTS afk (
     user_id BIGINT PRIMARY KEY,
     reason TEXT
 )
+
 CREATE TABLE IF NOT EXISTS todo (
     user_id BIGINT,
     task TEXT,
     jump_url TEXT
 )
+
 CREATE TABLE IF NOT EXISTS blacklist (
     user_id BIGINT PRIMARY KEY,
     reason TEXT
+)
+
+CREATE TABLE IF NOT EXISTS voice (
+    guild_id BIGINT,
+    user_id BIGINT,
+    channel_id BIGINT,
+    'enabled' BOOLEAN,
 )

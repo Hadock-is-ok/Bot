b1035b42d5201445b42959232d052f635d69cd9d
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-02-06 21:04:00 +0000

    cooliuo

--- ext/voice.py
+++ ext/voice.py
@@ -30,10 +30,13 @@ class Voice(commands.Cog):
         if not state.channel.members:
             channel = state.channel
             try:
-                owner = (bot.get_guild(member.guild.id)).get_member(vc.get(state.channel.id))
-                message = await owner.send("I will delete this channel for inactivity in 5 minutes if it's not used!")
-                await self.bot.wait_for("voice_join", timeout=300, check=channel_check)
-                await message.delete()
+                try:
+                  owner = (bot.get_guild(member.guild.id)).get_member(vc.get(state.channel.id))
+                  message = await owner.send("I will delete this channel for inactivity in 5 minutes if it's not used!")
+                  await self.bot.wait_for("voice_join", timeout=300, check=channel_check)
+                  await message.delete()
+                except Exception as error:
+                    print("fuck this fucking error called", error)
             except asyncio.TimeoutError:
                 try:
                     await channel.delete()

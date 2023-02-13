43d4fef0acb06c52f4d52df5bed0a154c24bc46a
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-02-13 19:44:03 +0000

    try without type checking

--- utils/bot.py
+++ utils/bot.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import datetime
 import logging
 import os
-from typing import TYPE_CHECKING, Any, ClassVar, Dict, List, NamedTuple, Optional, TypedDict, Union
+from typing import Any, ClassVar, Dict, List, NamedTuple, Optional, TypedDict, Union
 
 import aiohttp
 import asyncpg
@@ -12,8 +12,7 @@ from cachetools import TTLCache
 from discord.ext import commands
 from typing_extensions import Self
 
-if TYPE_CHECKING:
-    from . import AloneContext
+from . import AloneContext
 
 
 class Todo(NamedTuple):

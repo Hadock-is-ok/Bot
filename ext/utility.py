6b8251dc133073734ecfd6670ec3045c37cc6e3e
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-03-08 17:46:33 +0000

    Update utility.py

--- ext/utility.py
+++ ext/utility.py
@@ -67,7 +67,7 @@ class Utility(commands.Cog):
         start: float = perf_counter()
         message: discord.Message = await ctx.reply("Pong!")
         end: float = perf_counter()
-        typing_ping: float = end - start
+        typing_ping: float = (end - start) * 1000
 
         start = perf_counter()
         await self.bot.db.execute("SELECT 1")

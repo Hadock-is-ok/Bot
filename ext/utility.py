27e99e0781821776139460f54061639568eba44c
NightSlasher35 <49624805+NightSlasher35@users.noreply.github.com>
2023-03-12 18:50:00 +0000

    fix a breaking bug

--- ext/utility.py
+++ ext/utility.py
@@ -253,7 +253,7 @@ class Utility(commands.Cog):
             if todo_number == number:
                 try:
                     user_todo.remove(todo)
-                    await self.bot.db.execute("DELETE FROM todo WHERE user_id = $1", ctx.author.id)
+                    await self.bot.db.execute("DELETE FROM todo WHERE user_id = $1 AND task = $2", ctx.author.id, todo.content)
                     await ctx.message.add_reaction(ctx.Emojis.check)
                 except KeyError:
                     await ctx.reply("That's not a task in your todo list!")

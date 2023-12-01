b4a15b109851b93c8bef21aad0094787200d498c
Hadock <49624805+NightSlasher35@users.noreply.github.com>
2023-12-01 15:11:33 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import os
 import pathlib
-from typing import Any, ClassVar, Dict, List, NamedTuple, Optional, TypedDict
+from typing import Any, ClassVar, Dict, List, NamedTuple, Optional
 
 import aiohttp
 import asyncpg
@@ -11,32 +11,14 @@ import discord
 from cachetools import TTLCache
 from discord.ext import commands
 
-from utils.context import AloneContext
+#from utils.context import AloneContext
 
 
-class TODO_DATA(NamedTuple):
+class TodoData(NamedTuple):
     content: str
     jump_url: str
 
-
-class DEFAULT_GUILD_CONFIG(TypedDict):
-    prefix: str
-    enabled: bool
-    voice_channel: int
-    voice_category: int
-    community_voice_channels: dict[int, int]
-
-
-BOILERPLATE_GUILD_CONFIG: DEFAULT_GUILD_CONFIG = {
-    "prefix": "Alone",
-    "enabled": False,
-    "voice_channel": 0,
-    "voice_category": 0,
-    "community_voice_channels": {},
-}
-
-
-class AloneBot(commands.AutoShardedBot):
+class AloneBot(commands.Bot):
     INITAL_EXTENSIONS: List[str] = []
     DEFAULT_PREFIXES: ClassVar[List[str]] = ["Alone", "alone"]
     owner_ids: List[int]
@@ -53,34 +35,31 @@ class AloneBot(commands.AutoShardedBot):
 
         self.owner_ids = [412734157819609090, 1022842005920940063]  # type: ignore
         self.blacklisted_users: Dict[int, str] = {}
-        self.bypass_cooldown_users: List[int] = []
         self.afk_users: Dict[int, str] = {}
-        self.todos: Dict[int, List[TODO_DATA]] = {}
+        self.todos: Dict[int, List[TodoData]] = {}
         self.user_prefixes: Dict[int, List[str]] = {}
-        self.guild_configs: Dict[int, DEFAULT_GUILD_CONFIG] = {}
+        self.guild_prefixes: Dict[int, str] = {}
         self.bot_messages_cache: TTLCache[discord.Message, discord.Message] = TTLCache(maxsize=2000, ttl=300.0)
 
         self.cooldown: commands.CooldownMapping[discord.Message] = commands.CooldownMapping.from_cooldown(
             1, 1.5, commands.BucketType.member
         )
         self.support_server: str = os.environ["bot_guild"]
+        self.log_webhook: str = os.environ["webhook_url"]
         self.github_link: str = os.environ["github"]
         self.maintenance: Optional[str] = None
 
         self.command_counter: int = 0
         self.launch_time = discord.utils.utcnow()
+        self.jeyy_key = os.environ["jeyy_key"]
 
     async def get_prefix(self, message: discord.Message, /) -> List[str] | str:
         prefixes: List[str] = self.DEFAULT_PREFIXES.copy()
-        user_prefixes: List[str] | None = self.user_prefixes.get(message.author.id)
-        if user_prefixes:
-            prefixes.extend(user_prefixes)
+        user_prefixes: List[str] = self.user_prefixes[message.author.id]
+        prefixes.extend(user_prefixes)
 
-        if message.guild and (guild_prefix := self.guild_configs.get(message.guild.id)) is not None:
-            prefixes.append(guild_prefix.get("prefix"))
-
-        if not message.guild:
-            prefixes.append("")
+        if message.guild and (guild_prefix := self.guild_prefixes.get(message.guild.id, None)):
+            prefixes.append(guild_prefix)
 
         return commands.when_mentioned_or(*prefixes)(self, message)
 
@@ -89,16 +68,15 @@ class AloneBot(commands.AutoShardedBot):
 
     async def setup_hook(self) -> None:
         self.db: asyncpg.Pool[Any] | Any = await asyncpg.create_pool(
-            host=os.environ["db_ip"],
-            port=int(os.environ["db_port"]),
-            user=os.environ["db_user"],
-            password=os.environ["db_pwd"],
-            database=os.environ["database"],
+            host=os.environ["database_ip"],
+            port=int(os.environ["database_port"]),
+            user=os.environ["database_user"],
+            password=os.environ["database_key"],
+            database=os.environ["database_name"],
         )
         if not self.db:
             raise RuntimeError("Couldn't connect to database!")
 
-        # Loading schema.sql
         with open("schema.sql") as file:
             await self.db.execute(file.read())
 
@@ -115,47 +93,24 @@ class AloneBot(commands.AutoShardedBot):
             else:
                 self.INITAL_EXTENSIONS.append(f"{'.'.join(tree)}.{file.stem}")
 
-        log_webhook: str | None = os.getenv("webhook_url")
-        if not log_webhook:
-            raise RuntimeError("Webhook isn't set in .env!")
-        self.log_webhook: str = log_webhook
 
         records: list[Any]
         records = await self.db.fetch("SELECT user_id, array_agg(prefix) AS prefixes FROM prefix GROUP BY user_id")
         self.user_prefixes = {user_id: prefix for user_id, prefix in records}
 
         records = await self.db.fetch("SELECT * FROM guilds")
-        for guild_id, prefix, voice_channel, voice_category, enabled in records:
-            guild = self.guild_configs.setdefault(guild_id, BOILERPLATE_GUILD_CONFIG)
-            guild["prefix"] = prefix
-            guild["voice_channel"] = voice_channel
-            guild["voice_category"] = voice_category
-            guild["enabled"] = enabled
-
-        records = await self.db.fetch("SELECT * FROM voice")
-        for guild_id, user_id, channel_id in records:
-            guild: DEFAULT_GUILD_CONFIG = self.guild_configs.setdefault(guild_id, BOILERPLATE_GUILD_CONFIG)
-            guild.setdefault("community_voice_channels", {})[channel_id] = user_id
+        self.guild_prefixes = {guild_id: prefix for guild_id, prefix in records}
 
         records = await self.db.fetch("SELECT * FROM todo")
         for user_id, content, jump_url in records:
-            self.todos.setdefault(user_id, []).append(TODO_DATA(content, jump_url))
+            self.todos.setdefault(user_id, []).append(TodoData(content, jump_url))
 
         records = await self.db.fetch("SELECT * FROM afk")
         self.afk_users = {user_id: reason for user_id, reason in records}
 
-        records = await self.db.fetch("SELECT * FROM mentions")
-        for user_id in records:
-            self.bypass_cooldown_users.append(user_id)
-
-        self.jeyy_key = os.environ["jeyy_key"]
-
     async def close(self) -> None:
         await self.session.close()
-
-        if self.db:
-            await self.db.close()
-
+        await self.db.close()
         await super().close()
 
     async def start(self, token: str, *, reconnect: bool = True) -> None:
@@ -169,16 +124,3 @@ class AloneBot(commands.AutoShardedBot):
 
     def is_blacklisted(self, user_id: int) -> bool:
         return user_id in self.blacklisted_users
-
-    def add_owner(self, user_id: int) -> None:
-        self.owner_ids.append(user_id)
-
-    def remove_owner(self, user_id: int) -> str | None:
-        try:
-            self.owner_ids.remove(user_id)
-        except ValueError:
-            return "There's no owner with that ID!"
-
-    def format_print(self, text: str) -> str:
-        fmt: str = discord.utils.utcnow().strftime("%x | %X") + f" | {text}"
-        return fmt
